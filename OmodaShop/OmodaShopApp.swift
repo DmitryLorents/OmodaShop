b48f9f565565b2ca0db54838bb1ce853d58f760a
Dmitry Lorents <zmey.task@gmail.com>
2024-05-02 13:54:16 +0300

    Initial Commit

---
+++ OmodaShop/OmodaShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmodaShopApp.swift
+//  OmodaShop
+//
+//  Created by Dmitry on 02.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct OmodaShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
