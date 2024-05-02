d67f6fe6b7cd6dde5808b5c06c80d68439dedc10
Dmitry Lorents <zmey.task@gmail.com>
2024-05-02 15:14:32 +0300

    Add elements

--- OmodaShop/ContentView.swift
+++ OmodaShop/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private enum Constants {
+        static let imageRatio = 345.0 / 198.0
+        static let heightRation = 0.5
+    }
+    @State var pickerIndex = 0
+    private var pickerLabel = ["C5", "S5", "S5 GT"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            
+            Color.blackApp
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                
+                    Image(.omodaLogo)
+                        .padding()
+                    Image(pickerLabel[pickerIndex], bundle: nil)
+                        .resizable()
+                        .aspectRatio(Constants.imageRatio, contentMode: .fit)
+                        .padding(EdgeInsets(top: 22, leading: 22, bottom: 37, trailing: 22))
+                    
+                    Picker(selection: $pickerIndex) {
+                        ForEach(0..<pickerLabel.count, id: \.self) {
+                            Text(pickerLabel[$0]).tag($0)
+                        }
+                    } label: {
+                        Text("")
+                    }
+                    .pickerStyle(.segmented)
+                    .padding(EdgeInsets(top: 0, leading: 22, bottom: 0, trailing: 22))
+                
+                RoundedRectangle(cornerRadius: 16)
+                    .fill(Color.white)
+                    .ignoresSafeArea()
+                    .frame(height: UIScreen.main.bounds.height * Constants.heightRation)
+                    .padding(EdgeInsets(top: 32, leading: 0, bottom: 0, trailing: 0))
+                    
+                
+
+                
+                
+            }
+            
         }
-        .padding()
+        
     }
 }
 
